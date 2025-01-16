e64d1369a675a04d07fa81667bd01924b44d6a21
metetrkn <metetrkn52@gmail.com>
2025-01-16 21:50:37 +0100

    Remove unnecessary files from version control

---
+++ src/test/java/se/mete/files/DaifinCurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mete.files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaifinCurrencyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
